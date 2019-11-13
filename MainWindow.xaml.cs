38409dc76f7dc2e1e25556795fc0b52ccdb5d882
Thien Ngo <ngokhanhthien@yahoo.com>
2019-11-13 16:35:35 -0700

    11.13a

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -66,30 +66,32 @@ namespace vr6Motion
             Dispatcher.Invoke(DispatcherPriority.Send, new UpdateUiTextDelegate(SerialDataProcess), data);
         }
 
-        int deadZone1 = 0;
-        int deadZone2 = 0;
+        int deadZone = 0;
+        int PWMrev = 0;
         
         private void SerialDataProcess(String data)
         {
             Debug.WriteLine(data);
 
             byte[] bytes = Encoding.ASCII.GetBytes(data);
-            Debug.WriteLine(char.ToString((char)bytes[0]));
+            //Debug.WriteLine(char.ToString((char)bytes[0]));
             if (char.ToString((char)bytes[0]) == "[")
             {
                 switch (char.ToString((char)bytes[1]))
                 {
-                    case "D":
-                        deadZone1 = (int)bytes[2];
-                        DeadZoneValue1.Text = deadZone1.ToString();
-                        deadZone2 = (int)bytes[3];
-                        DeadZoneValue2.Text = deadZone2.ToString();
-                        
+                    case "V":
+                        deadZone = (int)bytes[2];
+                        Debug.Write(deadZone);
+
+                        DeadZoneValue.Content = deadZone.ToString();
+                        PWMrev = (int)bytes[3];
+                        Debug.Write(PWMrev);
+                        PWMrevValue.Content = PWMrev.ToString();
                         break;
                 }
 
             }
-           
+
         }
 
 
@@ -102,7 +104,7 @@ namespace vr6Motion
                 enabledControls(true);
                 //string selectedPort = comboxSelectPort.SelectedItem.ToString();
                 
-                port = new SerialPort("COM14", 19200, Parity.None, 8, StopBits.One);
+                port = new SerialPort("COM7", 19200, Parity.None, 8, StopBits.One);
                 port.DataReceived += new SerialDataReceivedEventHandler(DataReceivedHandler);
                 port.Open();
                 //port.Write("START\n");
@@ -135,10 +137,10 @@ namespace vr6Motion
         private void enabledControls(bool state)
         {
             led13CheckBox.IsEnabled = state;
-            DeadZone1N.IsEnabled = state;
-            DeadZone1P.IsEnabled = state;
-            DeadZone2N.IsEnabled = state;
-            DeadZone2P.IsEnabled = state;
+            DeadZoneN.IsEnabled = state;
+            DeadZoneP.IsEnabled = state;
+            ClipInputN.IsEnabled = state;
+            ClipInputP.IsEnabled = state;
         }
         
         private void led13CheckBox_Checked(object sender, RoutedEventArgs e)
@@ -153,18 +155,19 @@ namespace vr6Motion
 
         
 
-        private void DeadZone1P_Click(object sender, RoutedEventArgs e)
+        private void DeadZoneP_Click(object sender, RoutedEventArgs e)
         {
             //int dz = 65;
             //char x= (char)dz;
             //DeadZoneValue.Text = DeadZoneValue.Text + 1;
             //String sendData = "[" + char.ToString(x) + char.ToString(x) + char.ToString(x) + "]";
-            
-            
-            int updDeadZone1 = deadZone1 + 1;
-            sendDataNum("D", updDeadZone1, deadZone2);
+
+
+            stepValue = Int32.Parse(StepValue.Content.ToString());
+            int updDeadZone = deadZone + stepValue;
+            sendDataNum("V", updDeadZone, PWMrev);
             port.Write("[sav]");
-            port.Write("[rdD]");
+            port.Write("[rdV]");
         }
 
         private void sendDataNum(string header, int val1, int val2)
@@ -177,13 +180,19 @@ namespace vr6Motion
             port.Write(sendData);
         }
 
-
-        private void DeadZone1N_Click(object sender, RoutedEventArgs e)
+        int stepValue;
+        private void DeadZoneN_Click(object sender, RoutedEventArgs e)
         {
-            int updDeadZone1 = deadZone1 - 1;
-            sendDataNum("D", updDeadZone1, deadZone2);
+
+            
+            
+            
+            stepValue = Int32.Parse(StepValue.Content.ToString());
+            int updDeadZone = deadZone - stepValue;
+            if (updDeadZone < 0) { updDeadZone = 0; }
+            sendDataNum("V", updDeadZone, PWMrev);
             port.Write("[sav]");
-            port.Write("[rdD]");
+            port.Write("[rdV]");
 
         }
 
@@ -201,7 +210,7 @@ namespace vr6Motion
         {
             if (connectPortBtn.Content == "Disconnect")
             {
-                port.Write("[rdD]");
+                //port.Write("[rdD]");
                 //port.Write("[rdb]");
                 //port.Write("[rdc]");
                 
@@ -215,12 +224,12 @@ namespace vr6Motion
             }
         }
 
-        private void DeadZone2N_Click(object sender, RoutedEventArgs e)
+        private void ClipInputN_Click(object sender, RoutedEventArgs e)
         {
 
         }
 
-        private void DeadZone2P_Click(object sender, RoutedEventArgs e)
+        private void ClipInputP_Click(object sender, RoutedEventArgs e)
         {
 
         }
@@ -228,12 +237,64 @@ namespace vr6Motion
         private void selectOption_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
             string val = comboxMotor.SelectedValue.ToString();
-            Debug.Write(val);
+            
             if(val == "1")
             {
-                port.Write("[rdA]");
+                //port.Write("[rdA]");
+                //port.Write("[rdD]");
+                //port.Write("[rdJ]");
+                //port.Write("[rdM]");
+                //port.Write("[rdP]");
+                //port.Write("[rdS]");
+                port.Write("[rdV]");
 
             }
+        }
+
+        private void MaxLimitP_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void MaxLimitN_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void PWMrevP_Click(object sender, RoutedEventArgs e)
+        {
+            stepValue = Int32.Parse(StepValue.Content.ToString());
+            int updDeadZone = deadZone + stepValue;
+            sendDataNum("V", updDeadZone, PWMrev);
+            port.Write("[sav]");
+            port.Write("[rdV]");
+        }
+
+        private void PWMrevN_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void StepP_Click(object sender, RoutedEventArgs e)
+        {
+            stepValue = Int32.Parse(StepValue.Content.ToString());
+            if (stepValue == 1)
+            {
+                stepValue = Int32.Parse(StepValue.Content.ToString()) + 4;
+            }
+            else
+            {
+                stepValue = Int32.Parse(StepValue.Content.ToString()) + 5;
+            }
+            
+            StepValue.Content = stepValue.ToString();
+        }
+
+        private void StepN_Click(object sender, RoutedEventArgs e)
+        {
+            stepValue = Int32.Parse(StepValue.Content.ToString()) - 5;
+            if (stepValue < 0) { stepValue = 1; }
+            StepValue.Content = stepValue.ToString();
         }
     }
 }
